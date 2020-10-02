61efa86ef663f56059d28dee780b2deec6b4b37d
siham ouzia <siha.ouzia@gmail.com>
2020-10-02 21:47:18 +0200

    ceci est la classe fraction et la classe main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world ");
+		Fraction fraction = new Fraction (1,2);
+		fraction.toString();
 	}
 
 }
