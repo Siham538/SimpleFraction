400ccca33abf7b6634f0ca30890bbefce703e74b
siham ouzia <siha.ouzia@gmail.com>
2020-10-04 16:36:40 +0200

    le constructeur a été ajouté  à la classe fraction

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world ");
-		Fraction fraction = new Fraction (1,2);
-		fraction.toString();
+		
+		Fraction fraction = new Fraction (2,4);
+		System.out.println("le nombre fractionnaire est : " + fraction.toString());
+		
 	}
 
 }
